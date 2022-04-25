0ee4104eb78e4ea3ef3cb0b289ea3e49564ae31a
berkayustunel <94222577+berkayustunel@users.noreply.github.com>
2022-04-25 19:18:57 +0300

    Improved movement

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ var bmb2 = new Image();
 bmb2.src = "images/bomb.png";
 
 var x = 220; var y = 350;
+var dx = 0; var dy = 0;
 var bx = 100; var by = 100;
 var bx2 = 400; var by2 = 100;
 
@@ -46,36 +47,42 @@ function move(e)
 {
     if(e.keyCode == 38 || e.keyCode == 87 || e.keyCode == 119)
     {
-        y -= 5;
-        if(y <= 0)
-        {
-            y = 0;
-        }
+        dy = -1;
     }
     else if(e.keyCode == 37 || e.keyCode == 65 || e.keyCode == 97)
     {
-        x -= 5;
-        if(x <= 0)
-        {
-            x = 0;
-        }
+        dx = -1;
     }
     else if(e.keyCode == 40 || e.keyCode == 83 || e.keyCode == 115)
     {
-        y += 5;
-        if(y >= 470)
-        {
-            y = 470;
-        }
+        dy = 1;
     }
     else if(e.keyCode == 39 || e.keyCode == 68 || e.keyCode == 100)
     {
-        x += 5;
-        if(x >= 470)
-        {
-            x = 470;
-        }
+        dx = 1;
     }
+}
+
+addEventListener("keyup", stop);
+
+function stop(e)
+{
+    if(e.keyCode == 38 || e.keyCode == 87 || e.keyCode == 119)
+    {
+        dy = 0;
+    }
+    else if(e.keyCode == 37 || e.keyCode == 65 || e.keyCode == 97)
+    {
+        dx = 0;
+    }
+    else if(e.keyCode == 40 || e.keyCode == 83 || e.keyCode == 115)
+    {
+        dy = 0;
+    }
+    else if(e.keyCode == 39 || e.keyCode == 68 || e.keyCode == 100)
+    {
+        dx = 0;
+    }   
 }
 
 function moveBombs()
@@ -111,6 +118,8 @@ function draw()
     if(explode == false)
     {
         ctx.drawImage(bg, 0, 0);
+        x += dx; if(x <= 0){x = 0}; if(x >= 470){x = 470};
+        y += dy; if(y <= 0){y = 0}; if(y >= 465){y = 465};
         ctx.drawImage(character, x, y);
         ctx.drawImage(bmb, bx, by);
         ctx.drawImage(bmb2, bx2, by2);
@@ -122,6 +131,7 @@ function draw()
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         explode = false;
         x = 220; y = 350;
+        dx = 0; dy = 0;
         bx = 100; by = 100;
         bx2 = 400; by2 = 100;
         dbx = Math.random() * 3 + 2;
