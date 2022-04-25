b6ea9140972e9b6af9e200317cbc994c64936f95
berkayustunel <94222577+berkayustunel@users.noreply.github.com>
2022-04-25 19:33:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,22 +18,22 @@ var dx = 0; var dy = 0;
 var bx = 100; var by = 100;
 var bx2 = 400; var by2 = 100;
 
-var dbx = Math.random() * 3 + 2;
+var dbx = Math.random() * 2 + 1;
 if(Math.random() >= 0.5)
 {
     dbx = -dbx;
 }
-var dby = Math.random() * 3 + 2;
+var dby = Math.random() * 2 + 1;
 if(Math.random() >= 0.5)
 {
     dby = -dby;
 }
-var dbx2 = Math.random() * 3 + 2;
+var dbx2 = Math.random() * 2 + 1;
 if(Math.random() >= 0.5)
 {
     dbx2 = -dbx2;
 }
-var dby2 = Math.random() * 3 + 2;
+var dby2 = Math.random() * 2 + 1;
 if(Math.random() >= 0.5)
 {
     dby2 = -dby2;
@@ -85,32 +85,6 @@ function stop(e)
     }   
 }
 
-function moveBombs()
-{
-    bx += dbx; by += dby;
-    if(bx < 0 || bx+bmb.width > canvas.width)
-    {
-        dbx = -dbx;
-        bx += dbx;
-    }
-    if(by < 0 || by+bmb.height > canvas.height)
-    {
-        dby = -dby;
-        by += dby;
-    }
-    bx2 += dbx2; by2 += dby2;
-    if(bx2 < 0 || bx2+bmb2.width > canvas.width)
-    {
-        dbx2 = -dbx2;
-        bx2 += dbx2;
-    }
-    if(by2 < 0 || by2+bmb2.height > canvas.height)
-    {
-        dby2 = -dby2;
-        by2 += dby2;
-    }
-}
-
 explode = false;
 
 function draw()
@@ -120,11 +94,38 @@ function draw()
         ctx.drawImage(bg, 0, 0);
         x += dx; if(x <= 0){x = 0}; if(x >= 470){x = 470};
         y += dy; if(y <= 0){y = 0}; if(y >= 465){y = 465};
+        bx += dbx; by += dby;
+        if(bx < 0 || bx+bmb.width > canvas.width)
+        {
+            dbx = -dbx;
+            bx += dbx;
+        }
+        if(by < 0 || by+bmb.height > canvas.height)
+        {
+            dby = -dby;
+            by += dby;
+        }
+        bx2 += dbx2; by2 += dby2;
+        if(bx2 < 0 || bx2+bmb2.width > canvas.width)
+        {
+            dbx2 = -dbx2;
+            bx2 += dbx2;
+        }
+        if(by2 < 0 || by2+bmb2.height > canvas.height)
+        {
+            dby2 = -dby2;
+            by2 += dby2;
+        }
         ctx.drawImage(character, x, y);
         ctx.drawImage(bmb, bx, by);
         ctx.drawImage(bmb2, bx2, by2);
         ctx.font = "15px Arial";
         ctx.fillText("Score: " + score, 400, 50);
+        score += 1;
+        if(((x+character.width >= bx) && (x <= bx+bmb.width) && (y+character.height >= by) && (y <= by+bmb.height)) || (x+character.width >= bx2) && (x <= bx2+bmb2.width) && (y+character.height >= by2) && (y <= by2+bmb2.height))
+        {
+            explode = true;
+        }
     }
     else
     {
@@ -134,22 +135,22 @@ function draw()
         dx = 0; dy = 0;
         bx = 100; by = 100;
         bx2 = 400; by2 = 100;
-        dbx = Math.random() * 3 + 2;
+        dbx = Math.random() * 2 + 1;
         if(Math.random() >= 0.5)
         {
             dbx = -dbx;
         }
-        dby = Math.random() * 3 + 2;
+        dby = Math.random() * 2 + 1;
         if(Math.random() >= 0.5)
         {
             dby = -dby;
         }
-        dbx2 = Math.random() * 3 + 2;
+        dbx2 = Math.random() * 2 + 1;
         if(Math.random() >= 0.5)
         {
             dbx2 = -dbx2;
         }
-        dby2 = Math.random() * 3 + 2;
+        dby2 = Math.random() * 2 + 1;
         if(Math.random() >= 0.5)
         {
             dby2 = -dby2;
@@ -158,17 +159,4 @@ function draw()
         score = 0;
     }
     requestAnimationFrame(draw);
-}
-
-function addScore()
-{
-    score += 1;
-}
-
-function gameOver()
-{
-    if(((x+character.width >= bx) && (x <= bx+bmb.width) && (y+character.height >= by) && (y <= by+bmb.height)) || (x+character.width >= bx2) && (x <= bx2+bmb2.width) && (y+character.height >= by2) && (y <= by2+bmb2.height))
-    {
-        explode = true;
-    }
 }
